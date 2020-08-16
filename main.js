258b2da20382706f7a83050269d0d98bfca5a22d
sbuchmiller <scottbuchmiller@gmail.com>
2020-08-15 18:19:08 -0700

    SB added icon and toolbar, toolbar functons not implemented, icon is placeholder

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const electron = require('electron');
 const url = require('url');
 const path = require('path');
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, Menu} = electron;
 
 app.on('ready', function(){
     //creates a new window
     mainWindow = new BrowserWindow({
-        webPreferences: {nodeIntegration: true}
+        webPreferences: {nodeIntegration: true}, //required to allow chromium to interact with electron
+        icon: path.join(__dirname, "assets/icon.jpg")  //sets icon to whatever icon.jpg is in assets
     });
     //loads the html content into the main window
     mainWindow.loadURL(url.format({
@@ -14,6 +15,38 @@ app.on('ready', function(){
         protocol: 'file:',
         slashes: true
     }));
+    //Sets window toolbar to toolbar created with toolBarTemplate
+    Menu.setApplicationMenu(Menu.buildFromTemplate(toolBarTemplate));
     //quit app when hit X
     mainWindow.on('close', function(){app.quit()});
 })
+
+
+const toolBarTemplate = [
+    {
+        label:'file',
+        submenu:[
+            {
+                label:'Add a program',
+                click(){
+                    addFileToLauncher();
+                }
+            },
+            {
+                label:'Remove a program',
+                click(){
+                    removeFileFromLauncher();
+                }
+            }
+        ]
+    }
+]
+
+function addFileToLauncher(){
+    //change this function to be able to add filed to launcher, this will be called if user clicks
+    //file -> add a program
+}
+function removeFileFromLauncher(){
+    //change this function to be able to select a file to remove from launcher, this will be called if user clicks
+    //file -> remove a program
+}
